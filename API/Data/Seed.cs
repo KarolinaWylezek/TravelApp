80de59cd98f73f2c24d72ee992ef6f40f627b421
Karolina_Wyl <karolina.v.wylezek@gmail.com>
2020-12-16 16:07:12 +0100

    Seeded database

---
+++ API/Data/Seed.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.Users.AnyAsync()) return;
+
+            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+        }
+
+        public static async Task SeedCities(DataContext context)
+        {
+            if (await context.Cities.AnyAsync()) return;
+
+            var cityData = await System.IO.File.ReadAllTextAsync("Data/CitySeedData.json");
+            var cities = JsonSerializer.Deserialize<List<City>>(cityData);
+
+             foreach (var city in cities)
+            {
+                context.Cities.Add(city);
+            }
+            await context.SaveChangesAsync();
+
+        }
+    }
+}
