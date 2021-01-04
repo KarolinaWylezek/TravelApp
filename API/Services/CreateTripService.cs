b8aeee19a15fb89239b1299708218e8fce80abc5
Karolina_Wyl <karolina.v.wylezek@gmail.com>
2021-01-04 21:27:44 +0100

    plan for many days for subtheme

--- API/Services/CreateTripService.cs
+++ API/Services/CreateTripService.cs
@@ -50,6 +50,8 @@ namespace API.Services
              var otherPlaces = (await _tripsRepository.GetSCategoryPlaces(createTripDto.Subcategory, cityId))
              .Where(x => x.Promotion == false).OrderByDescending(x => x.Rating);
 
+             var places = placesWithPromotions.Concat(otherPlaces);
+
             // var otherPlaces = _mapper.Map<ICollection<AttractionDto>>((await _tripsRepository.GetSCategoryPlaces(createTripDto.Subcategory, cityId))
             //  .Where(x => x.Promotion == false).OrderByDescending(x => x.Rating));
 
@@ -60,86 +62,97 @@ namespace API.Services
 
             IEnumerable<EventDto> eventsToReturn = new Collection<EventDto>();
 
-            DateTime startTime = DateTime.ParseExact(createTripDto.StartOfSightseeing, "HH:mm", CultureInfo.InvariantCulture);
-            DateTime finishTime = DateTime.ParseExact(createTripDto.FinishOfSightseeing, "HH:mm", CultureInfo.InvariantCulture);
+           
 
             TimeSpan routeBreak = new TimeSpan(0, 30, 0);
-            int checker = 0;
+            
 
             // AttractionDto attr = _mapper.Map<AttractionDto>(placesWithPromotions.ElementAt<PlaceDto>(0));
 
             // attractions.Add(attr);
 
+            int tripDay = 0;
+
+            int noOfDays = (int)(createTripDto.TripFinishDate - createTripDto.TripDate).TotalDays;
+           
+
             
-
-
-            while (startTime < finishTime)
+            while (tripDay <= noOfDays)
             {
-                foreach (var item in events)
+                int checker = 0;
+                DateTime startTime = DateTime.ParseExact(createTripDto.StartOfSightseeing, "HH:mm", CultureInfo.InvariantCulture);
+                DateTime finishTime = DateTime.ParseExact(createTripDto.FinishOfSightseeing, "HH:mm", CultureInfo.InvariantCulture);
+
+                 while (startTime < finishTime)
                 {
-                    if (item.StartTime <= DateTime.ParseExact((createTripDto.TripDate.ToString("dd.MM.yyyy") + " " + startTime.ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture))
+                    foreach (var item in events)
                     {
-                        AttractionDto att = _mapper.Map<AttractionDto>(item);
-                        if(attractions.Any(x => x.Name == att.Name))
-                            continue;
+                        if (item.StartTime <= DateTime.ParseExact(((createTripDto.TripDate.AddDays(tripDay)).ToString("dd.MM.yyyy") + " " + startTime.ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture))
+                        {
+                            AttractionDto att = _mapper.Map<AttractionDto>(item);
+                            if(attractions.Any(x => x.Name == att.Name))
+                                continue;
                             else
                             {
-                                att.DateTimeOfVisit = DateTime.ParseExact((createTripDto.TripDate.ToString("dd.MM.yyyy") + " " + startTime.ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
+                                att.DateTimeOfVisit = DateTime.ParseExact(((createTripDto.TripDate.AddDays(tripDay)).ToString("dd.MM.yyyy") + " " + startTime.ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
                                 attractions.Add(att);
                                 startTime = startTime + TimeSpan.Parse(item.ExpectedTimeSpent) + routeBreak;
+                               
                             }
                         
+                        }
+
+                        else if( item.StartTime <= DateTime.ParseExact(((createTripDto.TripDate.AddDays(tripDay)).ToString("dd.MM.yyyy") + " " + (startTime.AddHours(3)).ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture))
+                        {
+                            AttractionDto att = _mapper.Map<AttractionDto>(item);
+                            if(attractions.Any(x => x.Name == att.Name))
+                                continue;
+                            else
+                            {
+                                att.DateTimeOfVisit = item.StartTime;
+                                attractions.Add(att);
+                                TimeSpan delay = DateTime.ParseExact(item.StartTime.ToString("HH:mm"), "HH:mm", CultureInfo.InvariantCulture) - startTime;
+                                startTime = startTime + TimeSpan.Parse(item.ExpectedTimeSpent) + routeBreak + delay;
+                            }
+                        }
                     }
-                }
 
-                if(startTime > finishTime)
-                    break;
+                    if(startTime > finishTime)
+                        break;
 
-                foreach (var place in placesWithPromotions)
-                {
-                    if ( DateTime.ParseExact(place.OpenTime, "HH:mm", CultureInfo.InvariantCulture) <= startTime && 
-                        startTime <=  DateTime.ParseExact(place.CloseTime, "HH:mm", CultureInfo.InvariantCulture) -  TimeSpan.Parse(place.ExpectedTimeSpent))
-                        {
-                            AttractionDto att = _mapper.Map<AttractionDto>(place);
-                            if(attractions.Any(x => x.Name == att.Name))
-                            continue;
-                            else{
-                                att.DateTimeOfVisit = DateTime.ParseExact((createTripDto.TripDate.ToString("dd.MM.yyyy") + " " + startTime.ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
-                                attractions.Add(att);
-                                startTime = startTime + TimeSpan.Parse(place.ExpectedTimeSpent) + routeBreak;
-                            }
+                    foreach (var place in places)
+                    {
+                        if ( DateTime.ParseExact(place.OpenTime, "HH:mm", CultureInfo.InvariantCulture) <= startTime && 
+                            startTime <=  DateTime.ParseExact(place.CloseTime, "HH:mm", CultureInfo.InvariantCulture) -  TimeSpan.Parse(place.ExpectedTimeSpent))
+                            {
+                                AttractionDto att = _mapper.Map<AttractionDto>(place);
+                                if(attractions.Any(x => x.Name == att.Name))
+                                    continue;
+                                else{
+                                    att.DateTimeOfVisit = DateTime.ParseExact(((createTripDto.TripDate.AddDays(tripDay)).ToString("dd.MM.yyyy") + " " + startTime.ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
+                                    attractions.Add(att);
+                                    startTime = startTime + TimeSpan.Parse(place.ExpectedTimeSpent) + routeBreak;
+                                    break;
+                                }
                             
                             
                                 
-                        }
-                }
-
-                if(startTime > finishTime)
-                    break;
-
-                foreach (var place in otherPlaces)
-                {
-                    if ( DateTime.ParseExact(place.OpenTime, "HH:mm", CultureInfo.InvariantCulture) <= startTime && 
-                        startTime <=  DateTime.ParseExact(place.CloseTime, "HH:mm", CultureInfo.InvariantCulture) -  TimeSpan.Parse(place.ExpectedTimeSpent))
-                        {
-                            AttractionDto att = _mapper.Map<AttractionDto>(place);
-                            if(attractions.Any(x => x.Name == att.Name))
-                            continue;
-                            else{
-                                att.DateTimeOfVisit = DateTime.ParseExact((createTripDto.TripDate.ToString("dd.MM.yyyy") + " " + startTime.ToString("HH:mm")), "dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
-                                attractions.Add(att);
-                                startTime = startTime + TimeSpan.Parse(place.ExpectedTimeSpent) + routeBreak;
                             }
-                            
-                            
-                                
-                        }
+                    }
+
+
+                    checker++;
+                    if(checker > 20)
+                        startTime += routeBreak;
+                    if(checker == 100) break;
+
                 }
-               checker++;
-               if(checker > 5)
-                startTime += routeBreak;
-               if(checker == 20) break;
+
+            tripDay++;
+
             }
+
+           
 
 
              return attractions;
